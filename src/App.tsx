5a69ad7393e1731fea7b19f609030206ee8bf3d4
Youngjun Ha <dudwnsk011@gmail.com>
2024-01-31 01:06:59 +0900

    Add CourseGoal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import CourseGoal from './components/CourseGoal.tsx';
+import goalsImg from './assets/goals.jpg';
+
 export default function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <main>
+      <Header image={{ src: goalsImg, alt: 'A list of goals' }} />
+      <CourseGoal title='Learn React + TS'>
+        <p>Learn it from the ground up</p>
+      </CourseGoal>
+    </main>
+  );
 }
